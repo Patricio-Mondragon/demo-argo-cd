1dc9fcc607ebc8e908b580f362739f06d7127110
Patricio-Mondragon <a01748352@itesm.mx>
2024-09-19 13:44:19 -0600

    Añadimos otro deployment a la rama prueba para poder diferenciar entre las dos

--- argo-script.sh
+++ argo-script.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <Namespace y path> <Rama del repo>"
+    exit 1
+fi
+
+
 # Variables
 ARGOCD_SERVER="openshift-gitops-server-openshift-gitops.apps.cluster-g87nx.sandbox2191.opentlc.com"      # URL del servidor ArgoCD
 ARGOCD_USERNAME="admin"                # Usuario de ArgoCD
 ARGOCD_PASSWORD="3OiIoM6wzQXLJt7vsApdxfh9Cyk4PNlY"             # Contraseña de ArgoCD
-APPLICATION_NAME="argo-demo"       # Nombre de la aplicación en ArgoCD
+APPLICATION_NAME="$1"       # Nombre de la aplicación en ArgoCD
 REPO_URL="https://github.com/Patricio-Mondragon/demo-argo-cd.git" # URL del repositorio Git
-REPO_PATH="dev"  # Path dentro del repo donde está la app
-DEST_NAMESPACE="argo-repo"          # Namespace en OpenShift donde se desplegará la app
+REPO_PATH="$1"  # Path dentro del repo donde está la app
+DEST_NAMESPACE="$1"          # Namespace en OpenShift donde se desplegará la app
 DEST_SERVER="https://kubernetes.default.svc" # El clúster de OpenShift
+BRANCH="$2" #Rama de la cual se tomará el path
+
 
 # Autenticación en ArgoCD
 argocd login $ARGOCD_SERVER --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD --insecure
@@ -18,7 +27,8 @@ argocd app create $APPLICATION_NAME \
     --repo $REPO_URL \
     --path $REPO_PATH \
     --dest-server $DEST_SERVER \
-    --dest-namespace $DEST_NAMESPACE
+    --dest-namespace $DEST_NAMESPACE \
+    --revision $BRANCH
 
 # Sincronizar la aplicación para desplegarla en el clúster de OpenShift
 argocd app sync $APPLICATION_NAME
